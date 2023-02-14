ab41c8795d5ecc71113153f8d3b2e1b51d336271
ThanzeerCould1 <115057527+ThanzeerCould1@users.noreply.github.com>
2023-02-14 21:47:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,14 @@ logger = logging.getLogger(__name__)
 import os
 
 from config import Config
-from pyrogram import Client 
+from pyrogram import SESSION,BOT_TOKEN,API_ID,API_HASH
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class autocaption(Client):
     
     def __init__(self):
         super().__init__(
-            session_name="SESSION",
+            name=SESSION,
             bot_token = Config.BOT_TOKEN,
             api_id = Config.API_ID,
             api_hash = Config.API_HASH,
@@ -27,4 +27,3 @@ class autocaption(Client):
 
 if __name__ == "__main__" :
     autocaption().run()
-
